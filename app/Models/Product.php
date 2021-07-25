49e3eeb849697710fe967324d354c46456f743f0
Łukasz Radziak <lukasilestam@gmail.com>
2021-07-25 23:02:15 +0200

    Add automatic slug to product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
--- database/migrations/2021_07_24_085353_create_products_table.php
+++ database/migrations/2021_07_24_085353_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
 
+            $table->string('slug')->nullable();
             $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->string('photo')->nullable();
